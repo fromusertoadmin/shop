f1d7800dde89cccbbd5d6479319ac51e8dd186ff
fromusertoadmin <setupvlad@gmail.com>
2017-08-30 16:09:20 +0300

    update3

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
 from .models import *
 from django.shortcuts import render
@@ -5,8 +6,14 @@ from .forms import CheckoutContactForm
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.db.models import Count, Prefetch
-from utils.emails import SendingEmail
+# from utils.emails import SendingEmail
 
+from django.template.loader import render_to_string, get_template
+from django.core.mail import EmailMessage
+
+from untitled6.settings import FROM_EMAIL, EMAIL_ADMIN
+from emails.models import EmailSendingFact
+from django.forms.models import model_to_dict
 
 def basket_adding(request):
     return_dict = dict()
@@ -89,3 +96,52 @@ def checkout(request):
         else:
             print("no")
     return render(request, 'orders/checkout.html', locals())
+
+
+class SendingEmail(object):
+    from_email = "BestShopEver <%s>" % FROM_EMAIL
+    reply_to_emails = [from_email]
+    target_emails = []
+    bcc_emails = []
+
+    def sending_email(self, type_id, email=None, order=None):
+
+        if not email:
+            email = EMAIL_ADMIN
+
+        target_emails = [email]
+
+        readyr = dict()
+
+        if type_id == 1:
+            subject = "Новый заказ"
+            readyr["order_fields"] = model_to_dict(order)
+            readyr["order"] = order
+            readyr["products_in_order"] = order.productinorder_set.filter(is_active=True)
+            message = get_template('emails_templates/order_notification_admin.html').render(readyr)
+
+        elif type_id == 2:
+            subject = 'Ваш заказ в итнтернет магазине получен!'
+            readyr["order_fields"] = model_to_dict(order)
+            readyr["order"] = order
+            readyr["products_in_order"] = order.productinorder_set.filter(is_active=True)
+            message = get_template('emails_templates/order_notification_customer.html').render(readyr)
+
+        msg = EmailMessage(
+                        subject, message, from_email=self.from_email, to=target_emails,
+                        bcc=self.bcc_emails, reply_to=self.reply_to_emails
+        )
+
+        msg.content_subtype = 'html'
+        msg.mixed_subtype = 'related'
+        msg.send()
+
+        kwargs = {
+            "type_id": type_id,
+            "email": email
+        }
+        if order:
+            kwargs["order"] = order
+        EmailSendingFact.objects.create(**kwargs)
+
+        print('Email was sent successfully!')
